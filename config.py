b1f1aac992635365ec81200fdc59616152200611
Novo-2 <72021185+Novo-2@users.noreply.github.com>
2025-12-11 22:11:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Числовой Telegram ID админа
 ADMIN_ID = 690609335
 
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")  # пример: HelpCaptainFF
-
+ADMIN_USERNAME = "fogcom"
 WAREHOUSE_ADDRESS = "Южнопортовая ул., 5, стр. 6"
 WAREHOUSE_MAP = "https://yandex.ru/maps/213/moscow/?ll=37.688854%2C55.707964&mode=routes&rtext=55.708237%2C37.688749&rtt=mt&ruri=&z=18.52"
 
 ADMIN_PHONE = os.getenv("ADMIN_PHONE")  # пример: +7 995 916 38 77
+
